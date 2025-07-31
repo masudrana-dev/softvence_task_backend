f208660682db95c8dae5481cc1775b9289fd341e
Masud Rana <masud.cphd@gmail.com>
2025-07-31 16:34:41 +0600

    linked

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(cors(
     {
-        origin: 'http://localhost:5173',
+        origin: ['http://localhost:5173' , `https://softvence-task-frontend.vercel.app/login`],
         credentials: true,
         methods: ['GET', 'POST', 'PUT', 'DELETE'],
         allowedHeaders: ['Content-Type', 'Authorization'],
